1babfc3daa1cb3678ed8cc7cd58340e2180042e1
piccolazucca <milena.zuccarelli@gmail.com>
2017-08-01 21:39:45 +0100

    created slider includes

--- resources/script.js
+++ resources/script.js
@@ -1,85 +1,9 @@
-// FLEX BOX CAROUSEL
-// taken from http://madewithenvy.com/ecosystem/articles/2015/exploring-order-flexbox-carousel/
-
-
-//one
-    $('#one-prev').on('click', function(e) {
-        handleEvent(e,'ul#one');
-    });
-    $('#one-next').on('click', function(e) {
-        handleEvent(e,'ul#one');
-    });
-//two
-    $('#two-prev').on('click', function(e) {
-        handleEvent(e,'ul#two');
-    });
-    $('#two-next').on('click', function(e) {
-        handleEvent(e,'ul#two');
-    });
-//three
-    $('#three-prev').on('click', function(e) {
-        handleEvent(e,'ul#three');
-    });
-    $('#three-next').on('click', function(e) {
-        handleEvent(e,'ul#three');
-    });
-//four
-    $('#four-prev').on('click', function(e) {
-        handleEvent(e,'ul#four');
-    });
-    $('#four-next').on('click', function(e) {
-        handleEvent(e,'ul#four');
-    });
-//five
-    $('#five-prev').on('click', function(e) {
-        handleEvent(e,'ul#five');
-    });
-    $('#five-next').on('click', function(e) {
-        handleEvent(e,'ul#five');
-    });
-//six
-    $('#six-prev').on('click', function(e) {
-        handleEvent(e,'ul#six');
-    });
-    $('#six-next').on('click', function(e) {
-        handleEvent(e,'ul#six');
-    });
-//seven
-    $('#seven-prev').on('click', function(e) {
-        handleEvent(e,'ul#seven');
-    });
-    $('#seven-next').on('click', function(e) {
-        handleEvent(e,'ul#seven');
-    });
-//eight
-    $('#eight-prev').on('click', function(e) {
-        handleEvent(e,'ul#eight');
-    });
-    $('#eight-next').on('click', function(e) {
-        handleEvent(e,'ul#eight');
-    });
-//nine
-    $('#nine-prev').on('click', function(e) {
-        handleEvent(e,'ul#nine');
-    });
-    $('#nine-next').on('click', function(e) {
-        handleEvent(e,'ul#nine');
-    });
-//ten
-    $('#ten-prev').on('click', function(e) {
-        handleEvent(e,'ul#ten');
-    });
-    $('#ten-next').on('click', function(e) {
-        handleEvent(e,'ul#ten');
-    });
-
 function handleEvent(e,parentNode) {
   var $seats = $(parentNode + ' li.carousel-seat')  
   var $newSeat;
   var $el = $(parentNode + ' li.is-ref');
   var $currSliderControl = $(e.currentTarget);
-  // Info: e.target is what triggers the event dispatcher to trigger and e.currentTarget is what you assigned your listener to.
-
+  
   $el.removeClass('is-ref');
   if ($currSliderControl.data('toggle') === 'next') {
     $newSeat = next($el);
